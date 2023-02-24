62b4c093aa978bc6fe2f2eaa7df96e848df68c15
emalasorte <109313583+emalasorte@users.noreply.github.com>
2023-02-24 01:30:43 +0000

    Esame Nuovo

--- esame.py
+++ esame.py
@@ -1,6 +1,6 @@
-# Compito d'esame Laboratorio: Emanuele Lasorte
-# Matricola: EC2100877
-# Programma per estrarre dati da un file CSV e calcolare la differenza giornaliera di temperatura 
+#Compito di laboratorio
+#Lasorte Emanuele EC2100877
+#Programma per estrarre dati da un file csv e  capire se dati due anni consecutivi c’è stata una variazione nel numero di passeggeri tra coppie di mesi quasi uguale (+-2), da un anno all’altro
 
 class ExamException(Exception):
     pass
@@ -32,51 +32,55 @@ class CSVTimeSeriesFile():
         # Itera ogni riga del file
         for line in my_file:
 
-            # Divide la riga in due elementi separati dalla virgola e limita la lunghezza della 
-            # lista a due elementi
+            # Divide la riga in due elementi separati dalla virgola e limita la lunghezza della lista a due elementi
             elements = line.strip().split(',')[:2]
 
             # Verifica se ci sono solo due elementi
             if len(elements) == 2:
 
-                # Estrae i due elementi (epoch e temperature)
-                epoch = elements[0]
-                temperature = elements[1]
+                # Estrae i due elementi (date e passeggeri)
+                dates = elements[0]
+                passengers = elements[1]
                 
                 try:
                     
-                    # Verifica se epoch è un numero intero
-                    epoch = int(float(epoch))
-
-                    # Verifica se temperature è un numero
-                    temperature = float(temperature)
+                    # Verifica se passeggeri è un numero intero
+                    passengers = int(float(passengers))
                     
                 except:
 
                     # Salta 
                     continue
 
-                # Verifica se temperature è un numero diverso da 0
-                if (isinstance(temperature,int) or isinstance(temperature,float) ) and temperature!=0:
+                # Verifica se passeggeri è intero
+                if (isinstance(passengers,int) and passengers>=0):
+                    # Serie di if per verificare la validità degli ANNI-MESI
+                    if '-' in elements[0]:
+                        test_mesi=elements[0].split('-')
+                        for i,item in enumerate(test_mesi):
+                            if (test_mesi[i].isnumeric()):
+                                if int(test_mesi[1]) in range(1,13):
+                                    
                     # Crea una nuova lista
-                    lista_nuova=[]
-                    lista_nuova.append(epoch)
-                    lista_nuova.append(temperature)
+                                    lista_nuova=[]
+                                    lista_nuova.append(dates)
+                                    lista_nuova.append(passengers)
+                                    
 
                     # Verifica se la lista è vuota
-                    if not lista_dati: 
-                        lista_dati.append(lista_nuova)
-                    
-                    else:
-
-                        # Verifica che gli epoch siano in ordine crescente
-                        lista_precedente=lista_dati[-1]
-                    
-                        if not epoch>lista_precedente[0]:
-                            raise ExamException('il timestamp è fuori ordine o duplicato.')
+                        if not lista_dati: 
+                            lista_dati.append(lista_nuova)
                             
                         else:
-                            lista_dati.append(lista_nuova)
+
+                        # Verifica che le date siano in ordine crescente
+                            lista_precedente=lista_dati[-1]
+                    
+                            if not dates>lista_precedente[0]:
+                                raise ExamException('il timestamp è fuori ordine o duplicato.')
+                            
+                            else:
+                                lista_dati.append(lista_nuova)
             else:
 
                 # Salta l'iterazione
@@ -91,161 +95,168 @@ class CSVTimeSeriesFile():
         
         return lista_dati
 
-# Ora la funzione vera e proprio che calcola la massima differenza giornaliera
-def compute_daily_max_difference(time_series=None):
 
-    # Verifica che il parametro sia stato inserito
-    if time_series is None: 
-        raise ExamException ("il parametro compute_daily_max_difference non è stato inserito!")
+#Funzione
 
-    # Verifica che il parametro sia una lista
-    if not isinstance(time_series,list):
-        raise ExamException("il parametro compute_daily_max_difference è inserito, ma non e' una lista!")
+def detect_similar_monthly_variations(time_series, years):
+    
+    # Controlla che l'input years sia una lista
+    if not isinstance(years, list):
+            raise ExamException('Errore, i valori non sono inseriti in una lista, ma si tratta di: {}'.format(type(years)))
 
-    # Verifica che la lista non sia vuota
-    if not time_series:
-        raise ExamException ("è stata inserita una lista vuota in compute_daily_max_difference!")
+    # Controlla che la lista years abbia esattamente due elementi
+    if (len(years)!=2):
+        raise ExamException('Errore, la lista inserita non può essere considerata perchè non ha due due valori ma ne ha: {}'.format(len(years)))
 
-    # Verifica che ogni elemento della lista sia una lista
+    # Controlla che entrambi gli elementi della lista years siano numeri interi
+    if (isinstance(years[0], int)==False or isinstance(years[1], int)==False):
+        raise ExamException('Errore, gli elementi della lista devono essere due numeri interi')
+
+    # Controlla che per entrambi gli anni esista almeno un dato nella time_series
+    for year in years:
+        if not any(x[0].startswith(str(year)) for x in time_series):
+            raise ExamException(f"Nessun dato disponibile per l'anno: {year}")
+
+    # Controlla che il primo anno sia inferiore al secondo
+    if(years[0]>=years[1]):
+        raise ExamException('Errore, range di anni non valido')
+
+    # Controlla che i due anni siano consecutivi
+    if(years[1]-years[0]!=1):
+        raise ExamException('Errore, range di anni non valido perchè i due anni inseriti devono essere consecutivi')
+
+
+    # Inizializza una lista vuota per contenere i dati
+    lista_dati = []
+
+    # Per ogni lista nella lista time_series
     for item in time_series:
-        if not isinstance(item,list):
-            raise ExamException('compute_daily_max_difference deve ricevere in input una lista di liste')
 
-    # Verifica che ogni sottolista abbia esattamente 2 elementi
-    for item in time_series:
-        if isinstance(item,list) and len(item) !=2:
-            raise ExamException('le sottoliste non sono di 2 elementi come richiesto')
-               
-    for i in range(len(time_series)):
+    # Inizializza una nuova lista vuota per contenere i dati della riga
+        riga_dati = []
 
-        # Verifica che la prima posizione della sottolista sia un intero o un float convertibile ad un intero
-        if isinstance(time_series[i][0],float):
-            time_series[i][0] = int(time_series[i][0])
-        
-        if not isinstance(time_series[i][0],int):
-            raise ExamException('ho trovato un epoch che non è numerico!')
+        # Per ogni elemento nella lista della riga
+        for i, element in enumerate(item):
 
-        # Verifica che gli epoch siano ordinati e non duplicati
-        if i>0:
-            if not time_series[i][0] > time_series[i-1][0]:
-                raise ExamException('gli epoch non sono ordinati o duplicati!')
+            # Se l'elemento è il primo, estrai l'anno e il mese e aggiungi alla riga dei dati
+            if i == 0:
+                anno_mese = element.split('-')
+                riga_dati.append(int(anno_mese[0]))
+                riga_dati.append(int(anno_mese[1]))
 
-        # Verifica che la seconda posizione della sottolista sia un numero
-        if not (isinstance(time_series[i][1],int) or isinstance(time_series[i][1],float)):
-            raise ExamException('la temperatura non è numerica: non accettabile!')
-
-        # Verifica che la temperatura non sia nulla
-        if time_series[i][1] == 0:
-            raise ExamException('la temperatura è nulla: non accettabile!')
-
-    # Inizializzazione della lista per contenere le statistiche totali
-    lista_differenze_totale = []
-
-    # Creazione di una lista per contenere i giorni
-    lista_giorni = []
-
-    # Iterazione sui dati delle serie temporali per estrarre i giorni 
-    for item in time_series:
-        lista_giorni.append(item[0]-(item[0]%86400))
-        
-    # Inizializzazione del contatore 'i'
-    i=0
-
-    # Loop che continua finché 'i' è inferiore alla lunghezza della lista dei giorni
-    while i<len(lista_giorni):
-
-        # Creazione della lista delle temperature per ogni giorno
-        lista_temperature=[time_series[i][1]]
-
-        # Inizializzazione della lista per le differenze giornaliere
-        differenze_giornaliere=[]
-
-        # Inizializzazione del contatore 'j' come 'i'+1
-        j=i+1
-
-        # Flag per indicare se siamo passati al giorno successivo
-        giorno_successivo=False
-
-        # Loop che continua finché 'j' è inferiore alla lunghezza della lista dei giorni
-        # e il giorno successivo non è ancora stato raggiunto
-        while j<len(lista_giorni) and giorno_successivo is False:
-
-        # Se il giorno corrente è uguale al giorno successivo, 
-        # aggiungi la temperatura del giorno successivo alla lista
-        # e incrementa 'j'
-            if lista_giorni[j] == lista_giorni[i]:
-                lista_temperature.append(time_series[j][1])
-                j+=1
-                
+            # Se l'elemento non è il primo, aggiungi il valore alla riga dei dati
             else:
+                riga_dati.append(element)
 
-                # Altrimenti, imposta 'i' come 'j' e imposta il flag a vero
-                i=j
-                giorno_successivo = True
-
-                # Come da istruzioni, se un giorno ha soltanto una misurazione il risultato deve essere None.
-                if len(lista_temperature) == 1:
-                    differenza_min_max = None
-
-                # Calcola il minimo, il massimo e la differenza tra il massimo e il minimo
-                # per la lista delle temperature del giorno
-                else:
-                
-                    minimo = min(lista_temperature)
-                    massimo = max(lista_temperature)
-                    differenza_min_max = massimo-minimo
-
-                # Aggiungi la differenza al giorno come differenza giornaliera
-                differenze_giornaliere = [differenza_min_max]
-
-                # Aggiungi le differenze giornaliere alla lista delle differenze totali
-                lista_differenze_totale.append(differenze_giornaliere)
-
-        # se l'indice "j" raggiunge la fine della lista "lista_giorni
-        if j == len(lista_giorni):
-
-            # Come da istruzioni, se un giorno ha soltanto una misurazione il risultato deve essere None.
-            if len(lista_temperature) == 1:
-                    differenza_min_max = None
-
-            else: 
-            # Calcola il minimo e il massimo delle temperature per il giorno corrente.
-                minimo = min(lista_temperature)
-                massimo = max(lista_temperature)
-                differenza_min_max = massimo-minimo
-
-            # Calcola la differenza tra il massimo e il minimo e la memorizza come differenza giornaliera.
-            differenze_giornaliere=[differenza_min_max]
-
-            # Aggiunge la differenza giornaliera alla lista totale delle differenze.
-            lista_differenze_totale.append(differenze_giornaliere)
-
-            # Aggiorna l'indice "i" per evitare di esaminare nuovamente il giorno corrente.
-            i=j
-
-    return lista_differenze_totale
+        # Aggiungi la riga dei dati alla lista dei dati
+        lista_dati.append(riga_dati)
 
 
-# Test
+    # Inizializza le liste vuote anno_0 e anno_1
+    anno_0 = []
+    anno_1 = []
 
+    # Scorri ogni lista nella lista di liste lista_dati
+    for item in lista_dati:
+        # Se l'anno nella lista corrente è uguale all'anno iniziale specificato
+        if item[0] == years[0]:
+            # Aggiungi alla lista anno_0 il mese e il valore della lista corrente
+            anno_0.append([item[1], item[2]])
+
+        # Se l'anno nella lista corrente è uguale all'anno finale specificato
+        if item[0] == years[1]:
+            # Aggiungi alla lista anno_1 il mese e il valore della lista corrente
+            anno_1.append([item[1], item[2]])
+
+
+        # Crea una lista di 12 elementi, tutti uguali a None, e assegnala alla variabile anno_0_passeggeri
+    anno_0_passeggeri = [None for i in range(12)]
+
+    # Crea una lista di 12 elementi, tutti uguali a None, e assegnala alla variabile anno_1_passeggeri
+    anno_1_passeggeri = [None for i in range(12)]
+
+
+        # Scorri tutti gli elementi della lista anno_0
+    for item in anno_0:
+        
+        # Estrai il valore del mese 
+        mese = int(item[0])
+        
+        # Assegna il valore del numero di passeggeri alla posizione corretta nella lista anno_0_passeggeri
+        anno_0_passeggeri[mese - 1] = item[1]
+
+    # Scorri tutti gli elementi della lista anno_1
+    for item in anno_1:
+        
+        # Estrai il valore del mese e sottrai 1 per ottenere l'indice corretto nella lista anno_1_passeggeri
+        mese = int(item[0])
+        
+        # Assegna il valore del numero di passeggeri alla posizione corretta nella lista anno_1_passeggeri
+        anno_1_passeggeri[mese - 1] = item[1]
+
+        
+    # Inizializzo due liste vuote che conterranno le differenze tra i passeggeri mensili dell'anno 0 e dell'anno 1
+    diff_anno_0=[]
+    diff_anno_1=[]
+
+    # Calcolo la differenza tra i passeggeri di ogni mese dell'anno 0
+    for i in range(11):
+    
+        # Se uno dei due mesi da confrontare non è disponibile, viene inserito None in diff_anno_0
+        if(anno_0_passeggeri[i]==None or anno_0_passeggeri[i+1]==None):
+            diff_anno_0.append(None)
+        # Altrimenti viene calcolata la differenza tra i due mesi e viene inserita in diff_anno_0
+        else:
+            diff_anno_0.append(anno_0_passeggeri[i+1]-anno_0_passeggeri[i])
+
+    # Stampo la lista contenente le differenze tra i passeggeri mensili dell'anno 0
+    print(diff_anno_0)
+
+    # Calcolo la differenza tra i passeggeri di ogni mese dell'anno 1
+    for i in range(11):
+    
+        # Se uno dei due mesi da confrontare non è disponibile, viene inserito None in diff_anno_1
+        if(anno_1_passeggeri[i]==None or anno_1_passeggeri[i+1]==None):
+            diff_anno_1.append(None)
+        # Altrimenti viene calcolata la differenza tra i due mesi e viene inserita in diff_anno_1
+        else:
+            diff_anno_1.append(anno_1_passeggeri[i+1]-anno_1_passeggeri[i])
+
+    # Stampo la lista contenente le differenze tra i passeggeri mensili dell'anno 1
+    # print(diff_anno_1)
+
+    # Inizializzazione di una lista vuota "risultato".
+    risultato = []
+
+    # Ciclo "for" che itera 11 volte.
+    # "range(11)" produce una sequenza di numeri da 0 a 10.
+    for i in range(11):
+    
+        # Verifica se uno dei valori nella lista "diff_anno_0" o "diff_anno_1" è None.
+        # Se uno dei valori è None, allora la condizione dell'"if" sarà vera, e l'elemento "False" sarà aggiunto alla lista "risultato".
+        if(diff_anno_0[i] == None or diff_anno_1[i] == None):
+            risultato.append(False)
+    
+        # Verifica se la differenza tra i valori nella lista "diff_anno_0" e "diff_anno_1" è compresa tra -2 e 2.
+        # Se la differenza rientra in questo intervallo, allora la condizione dell'"elif" sarà vera, e l'elemento "True" sarà aggiunto alla lista "risultato".
+        elif(diff_anno_0[i] - diff_anno_1[i] in range(-2, 3)):
+            risultato.append(True)
+    
+        # Se nessuna delle condizioni precedenti è vera, allora l'elemento "False" sarà aggiunto alla lista "risultato".
+        else:
+            risultato.append(False)
+
+    # La funzione restituisce la lista "risultato".
+    return risultato
+
+
+    
+    
+# Utilizzo
 """
-try:
-
-    print('\n...apertura del file csv in corso...\n')
-    time_series_file = CSVTimeSeriesFile(name="data.csv")
-    time_series=time_series_file.get_data()
-    
-    print('>> Operazione avvenuta con successo!')
-    print('>> Rilevazioni importate: {}'.format(len(time_series)))
-
-
-    differenze=compute_daily_max_difference(time_series)
-    
-    print('>>Stampa delle differenze massime giornaliere:')
-    
-    for item in differenze:
-        print('',(item[0]))
-
-except ExamException as e:
-    print("C'è stato un problema: {}".format(e))
+csv_file=CSVTimeSeriesFile('data.csv') 
+time_series=csv_file.get_data()
+lista=[1949,1954]
+result=detect_similar_monthly_variations(time_series,lista)
+print(result)
 """
